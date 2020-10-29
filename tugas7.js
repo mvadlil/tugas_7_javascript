ebed1ee7bce168802430251fd7057c15d3244818
Muhammad Fadlil <fadlilmuhammad27@gmail.com>
2020-10-29 18:38:14 +0700

    Tugas 7 Javascript
    Tugas ini untuk niomic bootcamp

---
+++ tugas7.js
@@ -0,0 +1,9 @@
+const tinggiBadan = [155, 150, 152, 169, 171, 165, 189, 158, 191, 158];
+
+for ( let x of tinggiBadan) {
+    console.log(x);
+}
+
+for ( var i = 0; i < tinggiBadan.length; i++ ) {
+    console.log(tinggiBadan[i]);
+}
